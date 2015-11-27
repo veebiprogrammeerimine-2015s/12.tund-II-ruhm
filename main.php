8e858dfb1b28d6a16ac465831b278b51c71cbc87
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-11-27 11:33:47 +0200

    login form

--- main.php
+++ main.php
@@ -34,34 +34,24 @@
 
 	<div class="row">
 	
-		<div class="col-md-6 col-md-offset-1">
+		<div class="col-md-6 col-md-offset-1 col-sm-8">
 			<div class="jumbotron">
 			  <h1>Hello, world!</h1>
 			  <p>...</p>
 			  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 			</div>
 		</div>
-		<div class="col-md-3 col-md-offset-1">
+		<div class="col-md-3 col-md-offset-1 col-sm-4">
+			<h3>Login</h3>
 			<form>
 			  <div class="form-group">
-				<label for="exampleInputEmail1">Email address</label>
 				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
 			  </div>
 			  <div class="form-group">
-				<label for="exampleInputPassword1">Password</label>
 				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 			  </div>
-			  <div class="form-group">
-				<label for="exampleInputFile">File input</label>
-				<input type="file" id="exampleInputFile">
-				<p class="help-block">Example block-level help text here.</p>
-			  </div>
-			  <div class="checkbox">
-				<label>
-				  <input type="checkbox"> Check me out
-				</label>
-			  </div>
-			  <button type="submit" class="btn btn-default">Submit</button>
+			  <button type="submit" class="btn btn-success pull-right hidden-xs">Submit</button>
+			  <button type="submit" class="btn btn-success btn-block visible-xs">MOBIIL</button>
 			</form>
 		</div>
 	
